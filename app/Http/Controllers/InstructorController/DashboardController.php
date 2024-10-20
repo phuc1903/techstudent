0da20f047b59f2e3de5d65e6c9887e3d0c725fd6
phuc1903 <dinhtrongphuc19@gmail.com>
2024-10-20 11:22:02 +0700

    Create branch

--- app/Http/Controllers/InstructorController/DashboardController.php
+++ app/Http/Controllers/InstructorController/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\InstructorController;
 
 use App\Http\Controllers\Controller;
+use App\Models\Instructor;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
